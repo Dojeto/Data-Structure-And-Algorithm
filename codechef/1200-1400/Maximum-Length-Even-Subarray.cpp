8a4b2e4ceb2c81404c4dcbeba6ec0fb1c455082f
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-09-27 22:10:18 +0530

    Update Maximum-Length-Even-Subarray.cpp

--- codechef/1200-1400/Maximum-Length-Even-Subarray.cpp
+++ codechef/1200-1400/Maximum-Length-Even-Subarray.cpp
@@ -28,3 +28,32 @@ int main() {
 	}
 	return 0;
 }
+
+// codechef problem :
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long int t;
+	cin>>t;
+	
+	while(t--){
+	   int n;
+	   cin>>n;
+	   int sum=0;
+	   int cnt=0;
+	   for(int i=1;i<=n;i++){
+	       sum+=i;
+	       cnt++;
+	   }
+	   if(sum%2==0){
+	       cout<<n<<endl;
+	   }else{
+	       cout<<cnt-1<<endl;
+	   }
+	}
+	return 0;
+}
+
