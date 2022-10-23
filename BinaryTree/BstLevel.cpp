6527c0411161d274c55f5cbce089e80e8d243f0e
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-10-23 15:59:02 +0530

    BstLevel

---
+++ BinaryTree/BstLevel.cpp
@@ -0,0 +1,14 @@
+  /* void printLevel(TreeNode* root,int level)
+    {
+        if(root == NULL)
+            return;
+        if(level == 1)
+        {
+            cout << root->val;
+        }
+        else
+        {
+            printLevel(root->left,level-1);
+            printLevel(root->right,level-1);
+        }
+    } */
