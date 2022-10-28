9117cdbc4e1bb6ad18dbb3b6a68d6979d3152190
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-10-28 14:12:38 +0530

    Update PowerOfTwo.cpp

--- leetcode/PowerOfTwo.cpp
+++ leetcode/PowerOfTwo.cpp
@@ -16,3 +16,19 @@ public:
         return checkPow(n);
     }
 };
+
+
+//using celi and floor 
+// log2(2^n) will give n so that mean it would be an pure int
+// celi means 1.4 = 2
+// floor means 1.4 = 1
+// so if ans will be in float that means celi and floor value will be different 
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0)
+            return false;
+        return floor(log2(n)) == ceil(log2(n))?true:false;
+    }
+};
