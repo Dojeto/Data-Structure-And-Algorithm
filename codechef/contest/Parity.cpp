aeacc670154319745e2203910c0a4fab5b2ff741
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-11-16 21:33:14 +0530

    Parity

---
+++ codechef/contest/Parity.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--)
+	{
+	    int x;
+	    cin >> x;
+	    if(x % 2 == 0)
+	    {
+	        cout << "YES" << endl;
+	    }
+	    else
+	    {
+	        cout << "NO" << endl;
+	    }
+	}
+	return 0;
+}
