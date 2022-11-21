203a71848227e4f950519ca956d640803e737b9e
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-11-21 16:10:28 +0530

    Update BinaryTreePreorderTraversal.cpp

--- BinaryTree/BinaryTreePreorderTraversal.cpp
+++ BinaryTree/BinaryTreePreorderTraversal.cpp
@@ -25,3 +25,36 @@ public:
         return arr;
     }
 };
+
+// using stack
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        if(root == NULL)return arr;
+        stack <TreeNode*> s;
+        s.push(root);
+        while(!s.empty())
+        {
+            TreeNode* temp;
+            temp = s.top();
+            s.pop();
+            arr.push_back(temp->val);
+            if(temp->right != NULL) s.push(temp->right);
+            if(temp->left != NULL) s.push(temp->left);
+        }
+        return arr;
+    }
+};
