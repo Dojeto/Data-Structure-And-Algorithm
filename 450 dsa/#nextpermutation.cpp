a5f21e2f230ae253c6eba40d15107bd2426f0c13
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-11-24 16:57:23 +0530

    Next Permutation

---
+++ 450 dsa/#nextpermutation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(nums.size() <= 1) return;
+        int i = nums.size() - 2;
+        while(i >= 0 && nums[i] >= nums[i+1]) i--;
+        if(i >=0)
+        {
+            int j = nums.size() -1;
+            while(nums[j] <=  nums[i]) j--;
+            swap(nums[i],nums[j]);
+        }
+        reverse(nums.begin() + (i+1),nums.end());
+        return; 
+    }
+};
