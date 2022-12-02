9b450bdac61a2929d99b19c65b7ecf5675426ff4
Om Limdiwala <81398258+Dojeto@users.noreply.github.com>
2022-12-02 22:45:01 +0530

    Build Array from Permutation

---
+++ leetcode/BuildArrayfromPermutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> arr(nums.size(),0);
+        for(int i=0;i<nums.size();i++)
+        {
+            arr[i] = nums[nums[i]];
+        }
+        return arr;
+    }
+};
